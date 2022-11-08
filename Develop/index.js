92cc4a8b3ade103785e9d85f9232637c623d2cc0
Rickelle <rickelle.1993@live.com.au>
2022-11-08 16:07:30 +1030

    Index.js-complete

--- Develop/index.js
+++ Develop/index.js
@@ -12,88 +12,79 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 // const filename = require("./node.js-README-generator/README.md")
 // TODO: Create an array of questions for user input
 //with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "What is the project title?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Write a brief description of your project: "
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "What command should be run to install dependencies? ",
-        },
-        {
-            type: "input",
-            name: "test",
-            message: "What command should be run to test?"
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "What is this project usage for?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Choose your license for this project: ",
-            choices: [
-                "Apach license 2.0",
-                "Mozilla Public License 2.0",
-                "MIT",
-                "GNU General Public License (GPL)",
-                "No License"
-            ]
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Who are the contributors of this projects?"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "What do I do if I have questions? "
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "What is your GitHub username: "
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your email: "
-        }
-    ])
-    .then((data) => {
-        console.log(data);
-   
-        // TODO: Create a function to write README file  
-        fs.writeFile('README.md', generateMarkdown(data), (err) =>
-            err ? console.log(err) : console.log('Generating README!')
-        );
-     });
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies? ",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to test?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project usage for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose your license for this project: ",
+        choices: [
+            "MIT",
+            "Apach license 2.0",
+            "Mozilla Public License 2.0",
+            "GNU General Public License (GPL)",
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have questions? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email: "
+    }
+]
 
 
 
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            console.log(data);
 
-
-// // TODO: Create a function to initialize app
-// function init() {
-//     questions() // Prompt user to get input data
-//         .then(data => {
-//             return generateMarkdown(data);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-// }
-// // // Function call to initialize app
-//  init();
+            // TODO: Create a function to write README file  
+            fs.writeFile('README.md', generateMarkdown(data), (err) =>
+                err ? console.log(err) : console.log('Generating README!')
+            );
+        });
+    };
+    // Function call to initialize app
+    init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // // If there is no license, return an empty string
 
-// function renderLicenseBadge(license) {
-//     if (license !== "No License") {
-//         return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
-//     }
-//     else if (license == "none" || "No license") {
-//         return "";
-//     }
-
+function rendersLicenseBadge(license) {
+    switch (license) {
+        case "Apache License 2.0":
+            return "[![License: Apache v2](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+        case "GNU General Public License v3.0":
+            return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+        case "MIT License":
+            return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+        case "Mozilla Public License 2.0":
+            return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]";
+        default:
+            return "";
+    }
+}
 //TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 
-// function renderLicenseLink(license) {
-
-// } else {
-//     return ''
-//   }
-// }
+function renderLicenseLink(license) {
+    switch (license) {
+        case "Apache License 2.0":
+            return "[![Link: Apache v2](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        case "GNU General Public License v3.0":
+            return "[![Link: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+        case "MIT License":
+            return "[![Link: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        case "Mozilla Public License 2.0":
+            return "[![Link: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+        default:
+            return "";
+    }
+}
 
 
 // TODO: Create a function that returns the license section of README
@@ -26,13 +40,20 @@
 //   if license
 // }
 
+
+
 // TODO: Create a function to generate markdown for README
 
 const generateMarkdown = (data) => {
     return `# ${data.title}
-<br/>
+
+    ${rendersLicenseBadge(data.license)}
+    <br/>
+    
     ![node.js badge](https://img.shields.io/badge/node.js%20-%2343853D.svg?&style=for-the-badge&logo=node.js&logoColor=white)
+    
     <br />
+
     ## Description
     ${data.description}
     ## Table of Contents
@@ -72,7 +93,9 @@ const generateMarkdown = (data) => {
     
     ## License 
     ${data.license}
-    
+     
+    ${renderLicenseLink(data.license)}
+
     ## Questions
     ${data.questions}
     
